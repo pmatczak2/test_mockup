fbf811e50a26642024aedc88294e28f46b33de85
peteykienzzz <pmatczak2@gmail.com>
2023-04-23 10:04:27 -0700

    mock files

---
+++ src/test_mockup/mock_date.py
@@ -0,0 +1,18 @@
+import datetime
+from unittest.mock import Mock
+
+tuesday = datetime.datetime(year=2019, month=1, day=1)
+saturday = datetime.datetime(year=2019, month=1, day=5)
+
+datetime = Mock()
+
+
+def is_weekday():
+    today = datetime.datetime.today()
+    return 0 <= today.weekday() < 5
+
+
+datetime.datetime.today.return_value = tuesday
+assert is_weekday()
+datetime.datetime.today.return_value = saturday
+assert not is_weekday()
