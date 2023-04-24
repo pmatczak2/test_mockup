4fb023dbdfb579f8e640119bd62ec6ac174481ee
peteykienzzz <pmatczak2@gmail.com>
2023-04-24 06:45:04 -0700

    added some notes to explain what's happening to the code in detail

--- src/test_mockup/mock_and_patch.py
+++ src/test_mockup/mock_and_patch.py
@@ -1,24 +1,30 @@
 from time import sleep
 from unittest.mock import patch
-
+# This imports the sleep function from the time module and the patch decorator from the unittest.mock module.
 
 # pretend I am an external function; I am problematic to test
 def calculate():
     sleep(10)
     return "y"
+# This defines a function named calculate that sleeps for 10 seconds and then returns the string "y". This function
+# is problematic to test because it takes a long time to execute due to the sleep call. I'm developing this; I use
+# external function
 
-
-# I'm developing this; I use external function
 def my_function():
     x = calculate()  # <- how to mock calculate() ?
     return x
-
+# This defines a function named my_function that calls the calculate function and returns its result. The calculate
+# function is used directly here, so it's not easy to test this function in isolation.
 
 def mytest():
     with patch("__main__.calculate") as calculate_mock:
         calculate_mock.return_value = "blah"
         assert my_function() == "blah"
-
+# This defines a test function named mytest that uses the patch decorator to temporarily replace the calculate
+# function with a mock object. The return_value attribute of the mock object is set to "blah", which means that the
+# calculate function will return "blah" instead of sleeping for 10 seconds and returning "y". The my_function is then
+# called within the assert statement and its result is compared to "blah", which should pass if everything works as
+# expected.
 
 if __name__ == "__main__":
     print("start running calculate")
@@ -28,3 +34,13 @@ if __name__ == "__main__":
     print("now test calculate")
     mytest()
     print("done")
+# This is the main part of the script. It starts by printing a message to indicate that the calculate function is
+# being called, then calls the calculate function directly. It then prints a message to indicate that the testing
+# phase is starting, and calls the mytest function which performs the test. Finally, it prints a message to indicate
+# that the script has finished.
+#
+# So in summary, this script defines a function calculate which takes a long time to execute, making it hard to test,
+# and a function my_function which uses calculate. A test function mytest is defined which uses the patch decorator
+# to temporarily replace calculate with a mock object that returns a known value instead of sleeping. This allows
+# my_function to be tested more easily. Finally, the script calls calculate and then mytest, and prints messages to
+# indicate the progress of the script.
