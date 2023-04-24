a32822ef52aa38a9df1eb536765157c2a9efb97d
peteykienzzz <pmatczak2@gmail.com>
2023-04-24 08:50:48 -0700

    added some notes to explain what's happening to the code in detail

--- src/test_mockup/mock_date.py
+++ src/test_mockup/mock_date.py
@@ -19,3 +19,25 @@ datetime.datetime.today.return_value = saturday
 assert not is_weekday()
 datetime.datetime.today.return_value = thursday
 assert is_weekday()
+
+# Mock is a powerful tool that can be useful in many different situations. Here are a few common scenarios where you
+# might want to use Mock:
+#
+# When you want to test code that depends on external resources (like a database or web service), but you don't want
+# your tests to actually interact with those resources. In this case, you can use Mock to create a fake version of
+# the external resource that behaves the way you want it to, just for the purposes of your test.
+#
+# When you want to test code that has side effects (like printing to the console or writing to a file), but you don't
+# want your tests to actually produce those side effects. In this case, you can use Mock to "catch" the output of the
+# code and check that it's what you expect.
+#
+# When you want to test code that depends on other functions, but those other functions are complicated or slow. In
+# this case, you can use Mock to create a fake version of the dependent function that behaves in a simple,
+# predictable way.
+#
+# So, you can use Mock whenever you want to simplify the testing process, by replacing complex or external
+# dependencies with simpler, more predictable versions that you control.
+#
+# However, it's important to use Mock judiciously. Overusing Mock can lead to tests that don't accurately reflect how
+# your code works in the real world. You should always aim to write tests that are as close to the real world as
+# possible, while still being efficient and reliable.
